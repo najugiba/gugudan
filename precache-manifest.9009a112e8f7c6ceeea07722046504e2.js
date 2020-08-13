dad681b3c932d2609452f2b555c84ecec72783f6
willis <kshwan1023@gmail.com>
2020-08-13 23:37:49 +0900

    Updates

---
+++ precache-manifest.9009a112e8f7c6ceeea07722046504e2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f7e1bb71c482104ca12ae0880e69f30",
+    "url": "/gugudan/index.html"
+  },
+  {
+    "revision": "37af68510ec4e9b23893",
+    "url": "/gugudan/static/css/main.6e4ee951.chunk.css"
+  },
+  {
+    "revision": "932295ef5a8477935e79",
+    "url": "/gugudan/static/js/2.cab64cab.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/gugudan/static/js/2.cab64cab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37af68510ec4e9b23893",
+    "url": "/gugudan/static/js/main.b025f027.chunk.js"
+  },
+  {
+    "revision": "0dfd0f5ccd72b0dfa75a",
+    "url": "/gugudan/static/js/runtime-main.23eeb030.js"
+  },
+  {
+    "revision": "9d7aae2da7a73e837935c69428dd91b0",
+    "url": "/gugudan/static/media/PoorStory-Regular.9d7aae2d.ttf"
+  },
+  {
+    "revision": "de7d8b298ca93530fe0608838a0fe6bd",
+    "url": "/gugudan/static/media/mongkey.de7d8b29.png"
+  }
+]);
